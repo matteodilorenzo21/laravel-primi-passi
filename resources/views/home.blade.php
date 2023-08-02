b53033d2c63e53701dd742640eea5ead845c1ae0
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-08-02 16:36:07 +0200

    added blade dynamic data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Primi Passi - Home</title>
+    <style>
+        h1 {
+            font-size: 50px;
+        }
+
+        span {
+            font-size: 35px;
+        }
+    </style>
 </head>
 
 <body>
-    <h1>Hello World!</h1>
+    <h1>Bulbasaur</h1>
+    <span><b>Tipo: </b>{{ $tipo }},</span><br>
+    <span><b>Prima abilità: </b>{{ $primaAbilita }},</span><br>
+    <span><b>Abilità speciale: </b>{{ $abilitaSpeciale }},</span><br>
+    <span><b>Sesso: </b>{{ $sesso }},</span><br>
+    <span><b>Altezza: </b>{{ $altezza }},</span><br>
+    <span><b>Peso: </b>{{ $peso }},</span><br>
+    <span><b>Tasso di cattura: </b>{{ $tassoDiCattura }}.</span>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $tipo = 'Erba - Veleno';
+    $primaAbilita = 'Erbaiuto';
+    $abilitaSpeciale = 'Clorofilla';
+    $sesso = '87.5% ♂, 12.5% ♀';
+    $altezza = '0,7 m';
+    $peso = '6,9 kg';
+    $tassoDiCattura = '45 (11.9%)';
+
+    return view('home', compact('tipo', 'primaAbilita', 'abilitaSpeciale', 'sesso', 'altezza', 'peso', 'tassoDiCattura'));
 });
